2256fa73a90acd490f3ab01b871971d7aa2affdf
Artur Racinowski <aracinowski@psi.pl>
2022-06-11 19:14:28 +0200

    Uruchomienie zdalnego klienta EJB, na osobnym hoście (przy pomocy dokeryzacji).

--- examples.JavaEE.client/src/main/java/RemoteEJBClient.java
+++ examples.JavaEE.client/src/main/java/RemoteEJBClient.java
@@ -1,12 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Hashtable;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import ejb.IInfo;
 
@@ -16,50 +12,17 @@ public class RemoteEJBClient {
 	private final static Hashtable jndiProperties = new Hashtable();
 
 	public static void main(String[] args) throws Exception {
-		Logger.getLogger("org.jboss").setLevel(Level.SEVERE);
-		Logger.getLogger("org.xnio").setLevel(Level.SEVERE);
-		testRemoteEJB();
-	}
+        System.out.println("Klient wystartował...");
 
-	private static void testRemoteEJB() throws NamingException {
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
         jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
-        final IInfo info = lookupInfoEJB();
 
-		dumpWelcomeMessage();
+        jndiProperties.put(Context.SECURITY_PRINCIPAL, "user");
+        jndiProperties.put(Context.SECURITY_CREDENTIALS, "user");
+
+        final Context context = new InitialContext(jndiProperties);
+        final IInfo info =  (IInfo) context.lookup("ejb:/examples.JavaEE.server-1.0-SNAPSHOT/Info!ejb.IInfo");
 
         logger.info(info.printInfo());
-	}
-
-	private static IInfo lookupInfoEJB() throws NamingException {
-        /*
-        Nazwa JDNI używana do wywołania bezstanowego ziarna sesyjnego.
-        ejb:<app-name>/<module-name>/<distinct-name>/<bean-name>!<fully-qualified-classname-of-the-remote-interface>
-
-        <app-name>
-        To nazwa aplikacji typu enterprise (bez elementu .ear), jeśli komponent EJB znajduje się w pakiecie EAR.
-
-        <module-name>
-        To nazwa modułu (bez elementu .jar lub .war), w którym znajduje się komponent EJB.
-
-        <distinct-name>
-        Można opcjonalnie ustawić nazwę wyróżniającą dla każdej jednostki wdrożenia.
-
-        <bean-name>
-        To nazwa klasy ziarna.
-
-        Stanowe komponenty EJB zawierają dodatkowy atrybut — ?stateful — na końcu tekstu JNDI,
-        więc pełny adres ma wówczas postać:
-        ejb:<app-name>/<module-name>/<distinct-name>/<bean-name>!<fully-qualified-classname-of-the-remote-interface>?stateful
-        */
-        final Context context = new InitialContext(jndiProperties);
-		return (IInfo) context.lookup("ejb:/examples.JavaEE.server-1.0-SNAPSHOT/Info!ejb.IInfo");
-
-        //ejb:/examples.JavaEE.server-1.0-SNAPSHOT/Info!ejb.IInfo
-
-	}
-
-	public static void dumpWelcomeMessage() {
-		System.out.println("Klient wystartował...");
 	}
 }
